0ec52d04557a7a814ddc789da93ed3aeaabe86a0
marienaomi.iwata <marienaomi.iwata@rakuten.com>
2018-06-18 06:27:01 +0000

    timeline fix

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -18,7 +18,7 @@ class MicropostsController extends Controller
         $data = [];
         if (\Auth::check()) {
             $user = \Auth::user();
-            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
 
             $data = [
                 'user' => $user,
@@ -27,6 +27,7 @@ class MicropostsController extends Controller
         }
         return view('welcome',$data);
     }
+    
     
     public function store(Request $request)
     {
@@ -40,6 +41,7 @@ class MicropostsController extends Controller
 
         return redirect()->back();
     }
+    
     
     public function destroy($id)
     {
